3379029117c8c86af06ea59fed99a52f9a044e7f
Linoy Cohen <45672703+linoyco@users.noreply.github.com>
2019-09-02 16:54:02 +0300

    start

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { CompanyComponent } from './components/company/company.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+
+@NgModule({
+  declarations: [ HomePageComponent, AdminComponent, CompanyComponent, CustomerComponent, LoginPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
